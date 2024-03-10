5f4684b727c223832624bb5c9b177f40ab786160
Anna Poglitsch <anna.poglitsch@outlook.de>
2024-03-10 13:44:48 +0100

    missing: movies

--- app/src/main/java/com/example/movieappmad24/MainActivity.kt
+++ app/src/main/java/com/example/movieappmad24/MainActivity.kt
@@ -4,8 +4,10 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -14,9 +16,14 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -28,6 +35,10 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -58,9 +69,11 @@ class MainActivity : ComponentActivity() {
                                 title = { Text(text = "MovieApp") },
                             )
                         },
+
                         bottomBar = {
                             BottomAppBar(modifier = Modifier.background(Color.Cyan)) {
                                 NavigationBarItem(
+                                    label = { Text(text = "Home") },
                                     selected = true,
                                     onClick = { /*TODO*/ },
                                     icon = {
@@ -70,15 +83,25 @@ class MainActivity : ComponentActivity() {
                                         )
                                     })
                                 NavigationBarItem(
+                                    label = { Text(text = "Watchlist") },
                                     selected = true,
                                     onClick = { /*TODO*/ },
-                                    icon = { Icon(
-                                        painter = painterResource(id = R.drawable.baseline_star_24),
-                                        contentDescription = null
-                                    ) })
+                                    icon = {
+                                        Icon(
+                                            painter = painterResource(id = R.drawable.baseline_star_24),
+                                            contentDescription = null
+                                        )
+                                    })
                             }
                         },
                         content = {
+                            Column(
+                                modifier = Modifier.fillMaxSize(),
+                                verticalArrangement = Arrangement.Center,
+                                horizontalAlignment = Alignment.CenterHorizontally
+                            ) {
+                                MovieCard()
+                            }
 
                         }
                     )
@@ -90,9 +113,10 @@ class MainActivity : ComponentActivity() {
     @Preview
     @Composable
     fun MovieCard() {
+        var expanded by remember { mutableStateOf(false) }
         Column(
             modifier = Modifier
-                .height(180.dp)
+                .height(if (expanded) 380.dp else 180.dp)
                 .width(350.dp)
                 .background(Color.White),
             horizontalAlignment = Alignment.CenterHorizontally,
@@ -130,26 +154,51 @@ class MainActivity : ComponentActivity() {
                     )
                 }
             }
-            Row(
+            
+                Row(
+                    modifier = Modifier
+                        .background(Color.LightGray)
+                        .animateContentSize()
+                        .width(350.dp)
+                        .height(30.dp)
+                        .padding(
+                            start = 10.dp,
+                        ),
+                ) {
+
+
+                    Text(
+                        text = "Avatar"
+                    )
+                    Spacer(modifier = Modifier.weight(1f))
+                    Button(
+                        onClick = { expanded = !expanded },
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = Color.Transparent,
+                            contentColor = Color.Black
+                        )
+                    ) {
+                        Icon(
+                            painter = painterResource(id = if (expanded) R.drawable.baseline_keyboard_arrow_down_24 else R.drawable.baseline_keyboard_arrow_up_24),
+                            contentDescription = null
+                        )
+                    }
+                }
+            Column(
                 modifier = Modifier
                     .background(Color.LightGray)
-                    .height(30.dp)
                     .width(350.dp)
                     .padding(
                         start = 10.dp,
-                        end = 10.dp,
                     ),
-            ) {
-
-
-                Text(
-                    text = "Avatar"
-                )
-                Spacer(modifier = Modifier.weight(1f))
-                Icon(
-                    painter = painterResource(id = R.drawable.baseline_keyboard_arrow_up_24),
-                    contentDescription = null
-                )
+            ) { if (expanded){
+                Text(text = "Director: ")
+                Text(text = "Released: ")
+                Text(text = "Genre: ")
+                Text(text = "Autors: ")
+                Text(text = "Rating: ")
+                Text(text = "Plot: ")
+            }
 
             }
         }
---
+++ app/src/main/java/com/example/movieappmad24/Movies.kt
@@ -0,0 +1,8 @@
+package com.example.movieappmad24
+
+data class Movies(
+    val id: String
+) {
+
+
+}
